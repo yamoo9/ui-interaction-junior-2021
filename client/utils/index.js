42a941bfa00b4fe339a8c8ed8342313b4f069d98
yamoo9 <yamoog@naver.com>
2021-11-13 13:49:06 +0900

    [학습] 유용한 유틸리티 (커스텀 함수) part 1.

--- client/utils/index.js
+++ client/utils/index.js
@@ -1,5 +1,13 @@
 /* JavaScript Utilities (Custom Functions) */
 
+const getRandomCount = (min = 0, max=10) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const throwError = (message) => {
+  throw new Error(message)
+};
+
 /* 데이터 타입 체크 유틸리티 ----------------------------------------------------------- */
 
 // - isUndefined()
@@ -9,19 +17,60 @@
 // - isNumber()
 // - isNaN()
 // - isBoolean()
+// - isTruthy()
+// - isFalsy()
 // - isFunction()
 // - isArray()
 // - isObject()
 // - isDate()
+// - isEqual()
+// - isNotEqual()
+
+const isUndefined = (data) => data === undefined;
+const isNull = (data) => data === null;
+const isNothing = (data) => isUndefined(data) || isNull(data);
+const isString = (data) => typeof data === 'string';
+const isNumber = data => typeof data === 'number' && !isNaN(data);
+const isBoolean = (data) => typeof data === 'boolean';
+const isFunction = (data) => typeof data === 'function';
+const isArray = (data) => Array.isArray(data);
+const isObject = (data) => data.constructor === Object;
+const isDate = (data) => data.constructor === Date;
 
 /* 데이터 타입 변경 유틸리티 ----------------------------------------------------------- */
 
 // - makeArray()
+const makeArray = (likeArrayObject) => {
+  if (Array.from) {
+    return Array.from(likeArrayObject);
+  } else if ('length' in likeArrayObject) {
+    // 반환 값: 배열
+    // 반환할 값을 선언
+    const returnValue = [];
+    for (let i = 0, l = 4; i<l; ++i) {
+      returnValue.push(likeArrayObject[i]);
+    }
+    return returnValue;
+  } else {
+    // 오류 출력: 유사 배열이 아님을 경고!
+    throwError('전달받은 데이터는 유사배열(like Array Object)가 아님!')
+  }
+}
 
 /* DOM 접근/조작 유틸리티 ------------------------------------------------------------ */
 
 // - getNode(), $()
 // - getNodeList(), $$()
+function getNodeList(selector, context = document) {
+  return makeArray(context.querySelectorAll(selector));
+}
+
+function getNode(selector, context = document) {
+  return getNodeList(selector, context)[0];
+}
+
+const $$ = getNodeList;
+const $ = getNode;
 
 // - getAttr()
 // - setAttr()
