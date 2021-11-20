943ad9d834a46d928e12fc9268d30282b898edb1
yamoo9 <yamoog@naver.com>
2021-11-20 11:30:56 +0900

    [학습] 속성, data 속성 설정 유틸리티 함수

--- client/utils/index.js
+++ client/utils/index.js
@@ -1,11 +1,11 @@
 /* JavaScript Utilities (Custom Functions) */
 
-const getRandomCount = (min = 0, max=10) => {
+const getRandomCount = (min = 0, max = 10) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
 const throwError = (message) => {
-  throw new Error(message)
+  throw new Error(message);
 };
 
 /* 데이터 타입 체크 유틸리티 ----------------------------------------------------------- */
@@ -30,7 +30,7 @@ const isUndefined = (data) => data === undefined;
 const isNull = (data) => data === null;
 const isNothing = (data) => isUndefined(data) || isNull(data);
 const isString = (data) => typeof data === 'string';
-const isNumber = data => typeof data === 'number' && !isNaN(data);
+const isNumber = (data) => typeof data === 'number' && !isNaN(data);
 const isBoolean = (data) => typeof data === 'boolean';
 const isFunction = (data) => typeof data === 'function';
 const isArray = (data) => Array.isArray(data);
@@ -47,15 +47,15 @@ const makeArray = (likeArrayObject) => {
     // 반환 값: 배열
     // 반환할 값을 선언
     const returnValue = [];
-    for (let i = 0, l = 4; i<l; ++i) {
+    for (let i = 0, l = 4; i < l; ++i) {
       returnValue.push(likeArrayObject[i]);
     }
     return returnValue;
   } else {
     // 오류 출력: 유사 배열이 아님을 경고!
-    throwError('전달받은 데이터는 유사배열(like Array Object)가 아님!')
+    throwError('전달받은 데이터는 유사배열(like Array Object)가 아님!');
   }
-}
+};
 
 /* DOM 접근/조작 유틸리티 ------------------------------------------------------------ */
 
@@ -69,6 +69,7 @@ function getNode(selector, context = document) {
   return getNodeList(selector, context)[0];
 }
 
+// 별칭(alias) 함수
 const $$ = getNodeList;
 const $ = getNode;
 
@@ -76,12 +77,40 @@ const $ = getNode;
 // - setAttr()
 // - attr()
 
+function hasProp(target, propName) {
+  return propName in target;
+}
+
+function isElementNode(node) {
+  if (!isNothing(node) && hasProp(node, 'nodeType')) {
+    if (node.nodeType === document.ELEMENT_NODE) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    throwError(
+      `전달된 ${node}는 존재하지 않거나, nodeType 속성을 가지고 있지 않습니다.`
+    );
+  }
+}
+
 function getAttr(node, attrName) {
-  return node.getAttribute(attrName);
+  if (isElementNode(node)) {
+    return node.getAttribute(attrName);
+  } else {
+    throwError('전달된 첫번째 인자는 HTML ElementNode가 아닙니다.');
+  }
 }
 
 function setAttr(node, attrName, value) {
-  node.setAttribute(attrName, value);
+  if (isElementNode(node) && isString(attrName)) {
+    node.setAttribute(attrName, value);
+  } else {
+    throwError(
+      '전달된 첫번째 인자는 HTML ElementNode가 아니거나 또는 두번 인자인 attrName이 문자 값이 아닙니다.'
+    );
+  }
 }
 
 function attr(node, attrName, value) {
@@ -91,6 +120,15 @@ function attr(node, attrName, value) {
 // - getData()
 // - setData()
 // - data()
+function getData(node, dataName) {
+  return getAttr(node, `data-${dataName}`);
+}
+function setData(node, dataName, value) {
+  setAttr(node, `data-${dataName}`, value);
+}
+function data(node, dataName, value) {
+  return attr(node, `data-${dataName}`, value);
+}
 
 // - addClass()
 // - removeClass()
